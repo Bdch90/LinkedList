ac8f9f5b3320fb4cba8eda58746f9505188f41b8
someone <someone@someplace.com>
2019-03-08 14:29:37 +0700

    new

---
+++ Main.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList(1);
+        linkedList.add(1,2);
+        linkedList.add(2,3);
+        linkedList.add(3,4);
+        linkedList.printList();
+    }
+}
